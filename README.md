5f8e71accc68905f0cf725e5a6c446e1fb3c214e
RyunHwaKang <rkdfbsghk@naver.com>
2019-03-06 00:07:51 +0900

    Test Commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Test-PA
 It's a repository just for test.
+
+name : 강륜화
+
+교수님 살려주세요 너무 어려워요
---
+++ TestPA/main.cpp
@@ -0,0 +1,7 @@
+#pragma warning(disable:4996)
+#include<stdio.h>
+
+void main() {
+	printf("Hello Git!\n");
+
+}
